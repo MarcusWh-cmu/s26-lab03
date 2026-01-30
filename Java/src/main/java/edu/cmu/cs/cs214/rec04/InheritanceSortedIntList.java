b3dc7f92a8d2a4b96efc2b20dff6a7df8e413a53
marcuswh <marcuswh@andrew.cmu.edu>
2026-01-30 01:01:53 -0500

    Delegation and Inheritance implementations complete, tests pass

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -11,7 +11,38 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
-
+    
+    private int totalAdded;
+    
+    /**
+     * Constructs a new InheritanceSortedIntList with a totalAdded count of 0.
+     */
+    public InheritanceSortedIntList() {
+        super();
+        this.totalAdded = 0;
+    }
+    
+    /**
+     * Adds the specified int to the list and increments the totalAdded count.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return super.add(num);
+    }
+    
+    /**
+     * Returns the total number of attempted element insertions to this list
+     * since it was created.
+     *
+     * @return the total number of elements added to the list
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
